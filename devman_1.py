63721348158c4581c22913d1fbe61e96ba1f0384
Rcenar <rcenar@mail.ru>
2023-07-19 23:13:12 +0700

    disable colors

--- devman_1.py
+++ devman_1.py
@@ -1,11 +1,7 @@
 import requests
 
 url_template = "https://wttr.in/{}?nTqu&lang=ru"
-
 locations = ["Лондон", "Шереметьево", "Череповец"]
-
-
-
 for location in locations:
-    r = requests.get(f"https://wttr.in/{location}?MnTq&lang=ru")
+    r = requests.get(f"https://wttr.in/{location}?MTnq&lang=ru")
     print(r.text)
